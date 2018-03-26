79da1d4d7609c342df92ee1773ea3de1aa37fda7
kenton cozart <kzart94@gmail.com>
2018-03-26 07:02:51 -0600

    reworks of Theta object to make it more intuitive to use, changed methods of accessing object's internals

--- neuralnet.py
+++ neuralnet.py
@@ -7,45 +7,74 @@ import numpy as np
 class Theta(object):
     #requires inputs of neural net specs (2,4,3)--number of nodes in each layer
     #optional input of the theta
-    def __init__(self, neural_net_specs):
+    def __init__(self, neural_net_specs, code = 0):
         #theta set to 0 as default
         self.specs = neural_net_specs
         #initialize random theta
-        self.theta_dict = {}
+        self.matrices = {}
+        if code == 0:
+            #if no input to set theta, then it sets theta to zeros
+            Theta.set_zeros(self)
+        elif code == 1:
+            #generate random theta with code 1
+            Theta.generate_random_theta(self)
 
-    def compile_theta(self, row_v):
-        temp_index = 0
-        #copied so that it does not point to the same location, and cannot be changed
-        #accidentally from outside of the class
-        row_vec = np.copy(row_v)
-        for i in range(len(self.specs)-1):
-            theta_dims = (self.specs[i+1], self.specs[i]+1)
-            num_values = theta_dims[0] * theta_dims[1]
-            self.theta_dict[i] = np.reshape(row_vec[temp_index:(temp_index+num_values)],theta_dims)
-            temp_index += num_values
+        #flattens theta upon initialization
+        Theta.flatten_theta(self)
+
+    def flatten_theta(self):
+        for i_theta in range(len(self.matrices)):
+            if i_theta == 0:
+                self.flat = self.matrices[i_theta].flatten()
+            else:
+                self.flat = np.concatenate((self.flat, self.matrices[i_theta].flatten()))
 
     def generate_random_theta(self, eps = 0.12):
         #generates a dictionary of random theta values, with a dictionary key
         #correlating to it's position in the neural network
+        #eps is the range in which the random values are generated (+/- eps)
         for i in range(len(self.specs)-1):
             theta_dims = (self.specs[i+1], self.specs[i]+1)
             random_theta = (np.random.rand(theta_dims[0],theta_dims[1]))*2*eps - eps
-            self.theta_dict[i] = random_theta
+            self.matrices[i] = random_theta
 
-    def flatten_theta(self):
-        #flattens theta matrix into row array
-        for i_theta in range(len(self.theta_dict)):
-            if i_theta == 0:
-                self.flattened_theta = self.theta_dict[i_theta].flatten()
-            else:
-                self.flattened_theta = np.concatenate((self.flattened_theta, self.theta_dict[i_theta].flatten()))
+    def set_theta(self, new_theta):
 
+        if type(new_theta) == list:
+            #convert to numpy array
+            new_theta = np.array(new_theta)
 
-    def get_theta_flat(self):
-        #makes sure that it reflattens theta, before returning the flattened theta
-        #this makes sure that it returns the most recent theta
+        if new_theta.size == new_theta.shape[0]:
+            #catches if it is a row vector
+
+            #counts the number of elements required, checks against input
+            count = 0
+            for i in range(len(self.specs)-1):
+                dims = Theta.required_theta_dims(self, i)
+                count += (dims[0] * dims [1])
+            if new_theta.size != count:
+                raise TypeError('Existing thetas and new theta have different number of values.')
+
+            #writes the input theta to the matrix structure
+            temp_index = 0
+            for i in range(len(self.specs)-1):
+                theta_shape = Theta.required_theta_dims(self, i)
+                num_values = theta_shape[0] * theta_shape[1]
+                self.matrices[i] = np.reshape(new_theta[temp_index:(temp_index+num_values)],theta_shape)
+                temp_index += num_values
+        else:
+            #throw error, maybe write some code later to handle matrix theta inputs
+            for i in range(0, (len(self.specs)-1)):
+                required_dims = Theta.required_theta_dims(self, i) #generates the acceptable size
+                #checks if there is an existing dictionary with the same key
+                if new_theta.shape == required_dims: #new theta is same shape as expected shape
+                    self.matrices[i] = new_theta
+                elif (required_dims[0] * required_dims[1]) == new_theta.size: #row vector
+                    self.matrices[i] = np.reshape(new_theta, required_dims)
+                else:
+                    raise TypeError('Incorrect size or shape for theta, based on the neural net specs.')
+        #flattens theta again
         Theta.flatten_theta(self)
-        return self.flattened_theta
 
     def required_theta_dims(self, dict_key):
         dict_key = int(dict_key) #forces it into an integer
@@ -54,57 +83,16 @@ class Theta(object):
         theta_dims = (self.specs[dict_key + 1], (self.specs[dict_key]+1))
         return theta_dims
 
-    def set_individual_theta(self, dict_key, new_theta):
-        if type(new_theta) == list:
-            #converts an input list to a NumPy array
-            new_theta = np.array(new_theta)
-        required_dims = Theta.required_theta_dims(self, dict_key) #generates the acceptable size
-
-        #checks if there is an existing dictionary with the same key
-        if dict_key in self.theta_dict.keys():
-            if new_theta.shape == required_dims: #new theta is same shape as expected shape
-                self.theta_dict[dict_key] = new_theta
-            elif (required_dims[0] * required_dims[1]) == new_theta.size: #row vector
-                self.theta_dict[dict_key] = np.reshape(new_theta, required_dims)
-            else:
-                raise TypeError('Incorrect size or shape for theta, based on the neural net specs.')
-
-        else:
-            #if the dictionary key doesn't exist, then make sure that it is either the right shape or size
-            if new_theta.shape == required_dims: #new theta is same shape as expected shape
-                self.theta_dict[dict_key] = new_theta
-            elif (required_dims[0] * required_dims[1]) == new_theta.size: #row vector
-                self.theta_dict[dict_key] = np.reshape(new_theta, required_dims)
-            else:
-                raise TypeError('Incorrect size or shape for theta, based on the neural net specs.')
-
-    def set_all_theta(self, row_vector):
-        #this resets all theta, requires a row vector or a list
-        if type(row_vector) == list:
-            #convert into a numpy array, if the input is a list
-            row_vector = np.array(row_vector)
-        count = 0
+    def set_zeros(self):
         for i in range(len(self.specs)-1):
-            dims = Theta.required_theta_dims(self, i)
-            count += (dims[0] * dims [1])
-        if row_vector.size != count:
-            raise TypeError('Existing thetas and new theta have different number of values.')
-        temp_index = 0
-        for i in range(len(self.specs)-1):
-            theta_shape = Theta.required_theta_dims(self, i)
-            num_values = theta_shape[0] * theta_shape[1]
-            self.theta_dict[i] = np.reshape(row_vector[temp_index:(temp_index+num_values)],theta_shape)
-            temp_index += num_values
-
-    def zeros(self):
-        for i in range(len(self.specs)-1):
-            self.theta_dict[i] = np.zeros((self.specs[i+1], (self.specs[i]+1)))
+            self.matrices[i] = np.zeros((self.specs[i+1], (self.specs[i]+1)))
 
     def get_theta_matrices(self):
-        return self.theta_dict
+        return self.matrices
 
     def __str__(self):
-        return str(self.theta_dict)
+        return str(self.matrices)
+
 
 def sigmoid(z):
     sig = (1.0 /(1.0+np.exp(-1.0 * z)))
@@ -153,8 +141,8 @@ def cost_and_grad(th, X, y, layer_specs, lam = 1):
     if type(X) == list:
         X = np.array(X)
     m = X.shape[0]
-    #converts theta input and retrieves the theta_dict
-    theta = th.get_theta_matrices()
+    #converts theta input and retrieves the matrices
+    theta = th.matrices
 
 
     #FEED FORWARD
@@ -204,8 +192,8 @@ def cost_and_grad(th, X, y, layer_specs, lam = 1):
     #initialize a gradient theta object
     #====================================================
     grad_theta = Theta(layer_specs)
-    grad_theta.zeros()
-    grad_th = grad_theta.get_theta_matrices()
+    grad_theta.set_zeros()
+    grad_th = grad_theta.matrices
 
     del_dict = {}
     for i in range((len(layer_specs)-1), 0, -1):
@@ -234,6 +222,32 @@ def cost_and_grad(th, X, y, layer_specs, lam = 1):
 
 
 
+def check_gradient(theta, X, y, layer_specs, lam = 1):
+    offset = 0.000001
+    #set theta to theta flat
+    #set an array of zeros, iterate through adding the offset, then run the numerical gradient
+    #iterate through and add the offset, run the
+
+
+    flat_theta = theta.get_theta_flat()
+    new_theta = Theta(layer_specs)
+    new_theta.set_all_theta(flat_theta)
+    zeros = np.zeros((1,theta.get_theta_flat().size)).flatten()
+    gradient_checked = np.zeros((1,theta.get_theta_flat().size)).flatten()
+    for i in range(int((zeros.size)/60)):
+        zeros[i] = offset
+        new_theta.set_all_theta(flat_theta-zeros)
+        (loss1, grad) = cost_and_grad(new_theta, X, y, layer_specs, lam)
+        new_theta.set_all_theta(flat_theta+zeros)
+        (loss2, grad) = cost_and_grad(new_theta, X, y, layer_specs, lam)
+
+        gradient_checked[i] = (loss2 - loss1) / (2 * offset)
+
+        zeros[i] -= offset #return to zeros all around
+
+
+    return gradient_checked
+
 
 mat_contents = scipy.io.loadmat('sampleData.mat')
 x_mat = mat_contents['X']
@@ -252,8 +266,31 @@ th = np.concatenate((theta1_flat, theta2_flat))
 node_spec = (400, 25, 10)
 
 theta_nn = Theta(node_spec)
-theta_nn.set_all_theta(th)
+theta_nn.set_theta(th)
 
 C_G = cost_and_grad(theta_nn, x_mat, y_mat, node_spec)
-print('Cost:', C_G[0])
-print('Gradients:', C_G[1])
+flat_grad = C_G[1].flat
+# print('Cost:', C_G[0])
+# print('Gradients:', flat_grad[1:30])
+#
+# checked_gradient = check_gradient(theta_nn, x_mat, y_mat, node_spec)
+# print(checked_gradient[1:30])
+
+import csv
+oData = []
+with open('OctaveData.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        oData.append(row)
+#turn oData into a numpy array
+#test parameters
+oData = np.array(oData)
+testTheta = oData[1:,0]
+testGrad = oData[1:,1]
+test_specs = (3,5,3)
+correct_theta = Theta(test_specs)
+correct_theta.set_theta(testTheta.transpose())
+print(correct_theta.flat)
+correct_grad = Theta(test_specs)
+correct_grad.set_theta(testGrad.transpose())
+print(correct_grad.flat)
