2582906c2843f09b4a7ab6aa8adc477dce9a43fb
kenton cozart <kzart94@gmail.com>
2018-04-17 07:43:59 -0600

    modularized code a bit more with functions to feed forward and backprop, wrote general structure of optimization using gradient descent

--- neuralnet.py
+++ neuralnet.py
@@ -2,155 +2,6 @@ import scipy, scipy.io
 import numpy as np
 import pandas as pd
 
-
-
-#import math
-#import sklearn as sk
-#NOTES: * is .* , and @ is matrix multiplication
-
-
-
-
-def sigmoid(z):
-    sig = (1.0 /(1.0+np.exp(-1.0 * z)))
-    return sig
-
-def sigmoidGradient(z):
-    sigGrad = sigmoid(z)*(1-sigmoid(z))
-    return sigGrad
-
-
-
-
-
-def cost_and_grad(theta, X, y, layer_specs, lam = 1):
-    '''
-
-    Description: this function computes the cost and the gradient of a neural net
-
-    INPUTS:
-    theta: Theta object defined above, weights for each layer
-    X: list or NumPy array of inputs, organized by columns being each x, rows being each example
-    y: NumPy logical array, rows of examples
-    layer_specs: (2, 4, 3) - (inputs, hidden layer nodes, outputs)
-    lam: Optional input, defines theta, normalization factor
-
-    OUTPUTS:
-    jTheta: Cost based on current weights
-    gradient: Gradients for each weight
-
-    '''
-
-    #convert X to a numpy array if needed
-    if type(X) == list:
-        X = np.array(X)
-    m = X.shape[0]
-    #converts theta input and retrieves the matrices
-
-    if type(y) == list:
-        y = np.array(y)
-    if y.shape[1] == 1:
-        #this means it isn't a logical array yet
-        y = logicalYMatrix(y)
-
-    #FEED FORWARD
-    #====================================================
-    a_dict = {} #dict of the nodes
-    z_dict = {} #dict of the intermediate values
-    #find value of hypothesis, h (loop based on number of layers), calculate, a, z
-
-    #FEED FORWARD
-    for layer in range(len(layer_specs)):
-        if layer == 0:
-            #first layer
-            ones = (np.ones((np.shape(X)[0],1)))
-            aVal = np.append(ones, X, axis = 1)
-            a_dict[layer] = aVal
-        elif layer == (len(layer_specs)-1):
-            #last layer
-            zVal = a_dict[(layer-1)] @ theta[(layer-1)].transpose()
-            aVal = sigmoid(zVal)
-            a_dict[layer] = aVal
-            h = aVal
-        else:
-            #middle layers
-            zVal = a_dict[layer-1] @ theta[layer-1].transpose()
-            z_dict[layer] = zVal
-            #calculate the sigmoid of the zValue
-            aVal = sigmoid(zVal)
-            #add a column of ones since it's a middle layer, accounting for bias unit
-            ones = (np.ones((np.shape(aVal)[0],1)))
-            aVal = np.append(ones, aVal, axis = 1)
-            a_dict[layer] = aVal
-    #====================================================
-
-    #CALCULATE COST BASED ON CURRENT THETA
-    #====================================================
-    jMatrix = ((-y * np.log(h))-((1-y)*np.log(1-h)))
-    jTheta = (1/m)*np.sum(jMatrix)
-
-    #adding in regularized terms, omitting theta bias terms
-    #====================================================
-    theta_sums = 0
-    for theta_i in range(len(theta.matrices)):
-        theta_sums += np.sum(np.square(theta[theta_i][:,1:]))
-    jTheta += (lam/(2*m))*theta_sums
-    #====================================================
-
-
-    #BACK PROPOGATION
-    #initialize a gradient theta object
-    #====================================================
-    grad_theta = Theta(layer_specs)
-    grad_theta.set_zeros()
-
-    del_dict = {}
-    for i in range((len(layer_specs)-1), 0, -1):
-
-        if i == (len(layer_specs)-1):
-            #outermost layer
-            del_val = h - y
-            del_dict[i] = del_val
-        else:
-            #inner layers
-            beta = del_val @ theta[i]
-            #throw out bias term
-            beta = beta[:,1:]
-            del_val = beta * sigmoidGradient(z_dict[i])
-            del_dict[i] = del_val
-
-
-        additive = (1/m) * (del_val.transpose() @ a_dict[i-1])
-        grad_theta[i-1] = grad_theta[i-1] + additive
-        reg_term = ((lam/m) * theta[i-1][:,1:])
-        grad_theta[i-1][:,1:] = grad_theta[i-1][:,1:] + reg_term
-        #grad_theta.set_theta_ind(grad_th[i-1], (i-1))
-        #print('Type of grad_th:', type(grad_th), '\n', grad_th)
-        #sets grad_theta object to the newly adjusted theta
-
-
-    #====================================================
-    grad_theta.flatten_theta()
-
-    return jTheta, grad_theta
-
-
-
-def logicalYMatrix(vectorY, numberLabels):
-    '''
-    creates a logical array for classification:
-    input: [[2],[3],[1]]
-    output: [[0,1,0],[0,0,1],[1,0,0]]
-    '''
-    if type(vectorY) == list:
-        vectorY = np.array(vectorY)
-
-    logicalArray = np.zeros((vectorY.shape[0],numberLabels))
-    for i in range(logicalArray.shape[0]):
-        logicalArray[i,(int(vectorY[i])-1)] = 1.0
-
-    return logicalArray
-
 class Theta(object):
     #requires inputs of neural net specs (2,4,3)--number of nodes in each layer
     def __init__(self, neural_net_specs, code = 0):
@@ -166,9 +17,9 @@ class Theta(object):
             Theta.generate_random_theta(self)
 
         #flattens theta upon initialization
-        Theta.flatten_theta(self)
+        Theta.get_flat(self)
 
-    def flatten_theta(self):
+    def get_flat(self):
         for i_theta in range(len(self.matrices)):
             if i_theta == 0:
                 self.flat = self.matrices[i_theta].flatten()
@@ -221,7 +72,7 @@ class Theta(object):
                 else:
                     raise TypeError('Incorrect size or shape for theta, based on the neural net specs.')
         #flattens theta again
-        Theta.flatten_theta(self)
+        Theta.get_flat(self)
         return self.matrices
 
     def set_theta_ind(self, new_th, new_key):
@@ -247,7 +98,7 @@ class Theta(object):
     def set_zeros(self):
         for i in range(len(self.specs)-1):
             self.matrices[i] = np.zeros((self.specs[i+1], (self.specs[i]+1)))
-        Theta.flatten_theta(self)
+        Theta.get_flat(self)
         #return self.matrices
 
     def get_theta_matrices(self):
@@ -264,30 +115,139 @@ class Theta(object):
     def __str__(self):
         return str(self.matrices)
 
+def sigmoid(z):
+    sig = (1.0 /(1.0+np.exp(-1.0 * z)))
+    return sig
+
+def sigmoid_gradient(z):
+    sigGrad = sigmoid(z)*(1-sigmoid(z))
+    return sigGrad
+
+def logical_y_matrix(vector_y, number_labels):
+    '''
+    creates a logical array for classification:
+    input: [[2],[3],[1]]
+    output: [[0,1,0],[0,0,1],[1,0,0]]
+    '''
+    if type(vector_y) == list:
+        vector_y = np.array(vector_y)
+
+    logical_y = np.zeros((vector_y.shape[0],number_labels))
+    for i in range(logical_y.shape[0]):
+        logical_y[i,(int(vector_y[i])-1)] = 1.0
+
+    return logical_y
+
+def feed_forward(theta, X, y, layer_specs):
+    m = y.shape[0]
+
+    a_dict = {} #dict of the nodes
+    z_dict = {} #dict of the intermediate values
+    #FEED FORWARD
+    for layer in range(len(layer_specs)):
+        if layer == 0:
+            #first layer
+            ones = (np.ones((np.shape(X)[0],1)))
+            aVal = np.append(ones, X, axis = 1)
+            a_dict[layer] = aVal
+        elif layer == (len(layer_specs)-1):
+            #last layer
+            zVal = a_dict[(layer-1)] @ theta[(layer-1)].transpose()
+            aVal = sigmoid(zVal)
+            a_dict[layer] = aVal
+            h = aVal
+        else:
+            #middle layers
+            zVal = a_dict[layer-1] @ theta[layer-1].transpose()
+            z_dict[layer] = zVal
+            #calculate the sigmoid of the zValue
+            aVal = sigmoid(zVal)
+            #add a column of ones since it's a middle layer, accounting for bias unit
+            ones = (np.ones((np.shape(aVal)[0],1)))
+            aVal = np.append(ones, aVal, axis = 1)
+            a_dict[layer] = aVal
+    #====================================================
+
+    #need to return hypothesis (h), a_dict, z_dict
+    return h, a_dict, z_dict
+
+def calculate_cost(theta, y, h, lam):
+    m = y.shape[0]
+    #CALCULATE COST BASED ON CURRENT THETA
+    #====================================================
+    jMatrix = ((-y * np.log(h))-((1-y)*np.log(1-h)))
+    jTheta = (1/m)*np.sum(jMatrix)
+
+    theta_sums = 0
+    for theta_i in range(len(theta.matrices)):
+        theta_sums += np.sum(np.square(theta[theta_i][:,1:]))
+    jTheta += (lam/(2*m))*theta_sums
+
+    return jTheta, jMatrix
+
+def backprop(theta, y, h, a_dict, z_dict, lam, layer_specs):
+
+    #initialize a gradient theta object
+    #====================================================
+
+    #THIS MIGHT NOT WORK
+    m = y.shape[0]
+    grad_theta = Theta(layer_specs)
+    grad_theta.set_zeros()
+
+    del_dict = {}
+    for i in range((len(layer_specs)-1), 0, -1):
+
+        if i == (len(layer_specs)-1):
+            #outermost layer
+            del_val = h - y
+            del_dict[i] = del_val
+        else:
+            #inner layers
+            beta = del_val @ theta[i]
+            #throw out bias term
+            beta = beta[:,1:]
+            del_val = beta * sigmoid_gradient(z_dict[i])
+            del_dict[i] = del_val
+
+
+        additive = (1/m) * (del_val.transpose() @ a_dict[i-1])
+        grad_theta[i-1] = grad_theta[i-1] + additive
+        reg_term = ((lam/m) * theta[i-1][:,1:])
+        grad_theta[i-1][:,1:] = grad_theta[i-1][:,1:] + reg_term
+        #grad_theta.set_theta_ind(grad_th[i-1], (i-1))
+        #print('Type of grad_th:', type(grad_th), '\n', grad_th)
+        #sets grad_theta object to the newly adjusted theta
+    #====================================================
+    return grad_theta
+
 def check_gradient(theta, X, y, layer_specs, lam = 3): #THIS DOESN'T WORK YET
-    offset = 0.000000001
+    offset = 0.00000001
     #set theta to theta flat
     #set an array of zeros, iterate through adding the offset, then run the numerical gradient
     #iterate through and add the offset, run the
 
     new_theta = Theta(layer_specs)
-    new_theta.set_theta(theta.flat)
+    new_theta.set_theta(theta.get_flat())
     delta_mat = Theta(layer_specs, 0)
-    delta = delta_mat.flat
+    delta = delta_mat.get_flat()
 
     num_grad_mat = Theta(layer_specs, 0)
-    num_grad = num_grad_mat.flat
+    num_grad = num_grad_mat.get_flat()
 
     for i in range(int(delta.size)):
         delta[i] = offset
-        new_theta.set_theta(theta.flat - delta)
-        loss1 = cost_and_grad(new_theta, X, y, layer_specs, lam)[0]
+        new_theta.set_theta(theta.get_flat() - delta)
+        h, a_values, z_values = feed_forward(new_theta, X, y, layer_specs)
+        loss1, cost_matrix = calculate_cost(new_theta, y, h, lam)
 
-        new_theta.set_theta(theta.flat + delta)
-        loss2 = cost_and_grad(new_theta, X, y, layer_specs, lam)[0]
 
-        #reset new_theta after processing to theta.flat
-        new_theta.set_theta(theta.flat)
+        new_theta.set_theta(theta.get_flat() + delta)
+        h, a_values, z_values = feed_forward(new_theta, X, y, layer_specs)
+        loss2, cost_matrix = calculate_cost(new_theta, y, h, lam)
+
+        #reset new_theta after processing to theta.get_flat()
+        new_theta.set_theta(theta.get_flat())
 
         #compute gradient here
         num_grad[i] = (loss2 - loss1) / (2 * offset)
@@ -295,32 +255,56 @@ def check_gradient(theta, X, y, layer_specs, lam = 3): #THIS DOESN'T WORK YET
         delta[i] = 0 #return to zeros all around
     num_grad_mat.set_theta(num_grad)
 
-    return theta, num_grad_mat
+    return num_grad_mat
+
+def grad_descent(theta, gradients, alpha):
+    '''
+    Takes one step in the gradient descent direction
+    '''
 
 
+    new_theta_flat = theta.get_flat() - alpha * gradients.get_flat()
+    theta.set_theta(new_theta_flat)
+    return theta
 
+def train_nn(X, y, layer_specs, lam, max_iter = 20 , eps_limit = 0.1, numerical_check = False):
+    '''
+    train the network's activation weights
 
-# mat_contents = scipy.io.loadmat('sampleData.mat')
-# x_mat = mat_contents['X']
-# y_vec = mat_contents['y']
-# y_mat = logicalYMatrix(y_vec,10)
-#
-# theta_contents = scipy.io.loadmat('ex4weights.mat')
-# theta1 = theta_contents['Theta1']
-# theta2 = theta_contents['Theta2']
-# theta2_flat = theta2.flatten()
-# theta1_flat = theta1.flatten()
-#
-#
-# th = np.concatenate((theta1_flat, theta2_flat))
-#
-# node_spec = (400, 25, 10)
-#
-# theta_nn = Theta(node_spec)
-# theta_nn.set_theta(th)
-#
-# C_G = cost_and_grad(theta_nn, x_mat, y_mat, node_spec)
-# print(C_G[0])
+    '''
+    #initialize a random theta
+    theta_train = Theta(layer_specs)
+    theta_train.generate_random_theta()
+
+    #feed forward, backpropagate and calculate cost
+    #keep iterating until you pass max_iter or eps drops below eps_limit
+    i = 0
+    eps = 1000 # default
+    temp_cost = 100000000000000
+    while (i <= max_iter):
+        h, a_values, z_values = feed_forward(theta_train, X, y, layer_specs)
+        theta_train_grad = backprop(theta_train, y, h, a_values, z_values, lam, layer_specs)
+        cost, cost_matrix = calculate_cost(theta_train, y, h, lam)
+        #print('Theta before grad_descent:', theta_train.get_flat())
+        #perform gradient descent, alter theta_train
+        theta_train = grad_descent(theta_train, theta_train_grad, 0.3)
+        #print('Theta after grad_descent:', theta_train.get_flat())
+        #update while loop conditional values
+        eps = (temp_cost - cost)
+        temp_cost = cost #set temp_cost for next iteration
+        i += 1
+
+        if numerical_check == True and i == 1:
+            #only checks the gradient on the first iteration
+            num_grad = check_gradient(theta_train, X, y, layer_specs, lam)
+            sum_difference = 0
+            for i in range(theta_train_grad.get_flat().shape[0]):
+                sum_difference += (theta_train_grad.get_flat()[i] - num_grad.get_flat()[i])
+            average_difference = sum_difference / theta_train_grad.get_flat().shape[0]
+            print('average difference between numerical gradient and backprop gradient:\n', average_difference)
+
+        print('cost after iteration', i-1, ':', cost)
+        print('eps:', eps)
 
 var_Data = pd.read_csv("lossData.csv", float_precision='round_trip')
 xyData = pd.read_csv("XYData.csv", float_precision='round_trip')
@@ -338,14 +322,13 @@ xyData = np.array(xyData)
 test_X = np.array(xyData[0:,:3])
 test_y = np.array((xyData[0:,3]))
 print()
-test_y = logicalYMatrix(test_y, 3)
-lambda_test = 3
-J, grad_nn = cost_and_grad(test_theta, test_X, test_y, test_specs, lambda_test)
-#theta, X, y, layer_specs, lam = 1
+test_y = logical_y_matrix(test_y, 3)
+test_lam = 3
 
+# h, a_values, z_values = feed_forward(test_theta, test_X, test_y, test_specs)
+# theta_train_grad = backprop(test_theta, test_y, h, a_values, z_values, test_lam, test_specs)
+# cost, cost_matrix = calculate_cost(test_theta, test_y, h, test_lam)
+#
+# print(cost)
 
-
-# I THINK I AM LOSING SOME FIDELITY WHEN GOING FROM PANDAS into NUMPY
-output_theta, num_gradient = check_gradient(test_theta, test_X, test_y, test_specs)
-print('numerical gradient:\n', num_gradient.flat)
-print('backprop gradient:\n', grad_nn.flat)
+train_nn(test_X, test_y, test_specs, test_lam, max_iter = 1000, numerical_check = False)
